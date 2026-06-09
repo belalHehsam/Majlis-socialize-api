36f3cdca32a75fd2494d889510dd25b923993c89
Ammar Ahmed <ammarahmed1263@gmail.com>
2026-06-09 17:29:13 +0300

    feat(notifications): add testing endpoint and socket trigger mechanism

--- src/features/notifications/notificationController.ts
+++ src/features/notifications/notificationController.ts
@@ -1,38 +1,35 @@
+import { Request, Response } from "express";
+import { AppError } from "../../utils/appError";
+import { assertUser } from "../../utils/assertUser";
 import { asyncWrapper } from "../../utils/asyncWrapper";
-import { Request, Response, NextFunction } from "express";
+import jsend from "../../utils/jsend";
 import {
   getUnreadNotificationCount,
   getUserNotifications,
   markAllRead,
   markOneRead,
 } from "./notificationService";
-import jsend from "../../utils/jsend";
-import { assertUser } from "../../utils/assertUser";
-import { AppError } from "../../utils/appError";
+import { seedTestNotificationsIfNeeded } from "../../socket/socketManager";
 
-export const getNotifications = asyncWrapper(
-  async (req: Request, res: Response) => {
-    assertUser(req);
+export const getNotifications = asyncWrapper(async (req: Request, res: Response) => {
+  assertUser(req);
 
-    const { page, limit, unreadOnly } = req.query as {
-      page?: number;
-      limit?: number;
-      unreadOnly?: boolean;
-    };
+  const { page, limit, unreadOnly } = req.query as {
+    page?: number;
+    limit?: number;
+    unreadOnly?: boolean;
+  };
 
-    const data = await getUserNotifications(req.user.id, { page, limit, unreadOnly });
-    res.status(200).json(jsend.success(data));
-  }
-);
+  const data = await getUserNotifications(req.user.id, { page, limit, unreadOnly });
+  res.status(200).json(jsend.success(data));
+});
 
-export const getUnreadCount = asyncWrapper(
-  async (req: Request, res: Response) => {
-    assertUser(req);
+export const getUnreadCount = asyncWrapper(async (req: Request, res: Response) => {
+  assertUser(req);
 
-    const count = await getUnreadNotificationCount(req.user.id);
-    res.status(200).json(jsend.success({ unreadCount: count }));
-  }
-);
+  const count = await getUnreadNotificationCount(req.user.id);
+  res.status(200).json(jsend.success({ unreadCount: count }));
+});
 
 export const markNotificationRead = asyncWrapper(async (req: Request, res: Response) => {
   assertUser(req);
@@ -54,4 +51,12 @@ export const markAllNotificationsRead = asyncWrapper(async (req: Request, res: R
   const result = await markAllRead(req.user.id);
 
   res.status(200).send(jsend.success(result));
+});
+
+export const triggerTestNotifications = asyncWrapper(async (req: Request, res: Response) => {
+  assertUser(req);
+
+  await seedTestNotificationsIfNeeded(req.user.id, true);
+
+  res.status(200).send(jsend.success({ message: "Test notifications triggered. Refresh or check socket." }));
 });
