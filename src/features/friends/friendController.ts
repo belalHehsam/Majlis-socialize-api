03b6a042163a16d36a2ba4c76914960ac3d55db2
Ahmed Alaa <ahmedalaa21500@gmail.com>
2026-06-13 02:43:07 +0300

    feat(users): respect notification and friend request settings across BE

--- src/features/friends/friendController.ts
+++ src/features/friends/friendController.ts
@@ -1,46 +1,61 @@
 import { Request, Response, NextFunction } from "express";
 import { asyncWrapper } from "../../utils/asyncWrapper";
-import {AppError} from "../../utils/appError";
+import { AppError } from "../../utils/appError";
 import jsend from "../../utils/jsend";
 import Friend from "../../models/Friend";
 import { createNotification } from "../notifications/notificationService";
+import User from "../../models/User";
 
 
-export const sendFriendRequest = asyncWrapper(async (req: Request, res: Response, next: NextFunction) => {
-  const user = req.user!; 
-  const { recipientId } = req.body;
-  const requesterId = user.id;
+export const sendFriendRequest = asyncWrapper(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const user = req.user!;
+    const { recipientId } = req.body;
+    const requesterId = user.id;
 
-  if (recipientId === requesterId) {
-    return next(new AppError("You cannot send a friend request to yourself", 400));
+    if (recipientId === requesterId) {
+      return next(new AppError("You cannot send a friend request to yourself", 400));
+    }
+
+    const recipient = await User.findOne({
+      _id: recipientId,
+      accountStatus: { $ne: "deleted" },
+    }).select("settings.allowFriendRequests");
+
+    if (!recipient) {
+      return next(new AppError("Recipient user not found", 404));
+    }
+
+    if (recipient.settings?.allowFriendRequests === false) {
+      return next(new AppError("This user is not accepting friend requests", 403));
+    }
+
+    const existingRequest = await Friend.findOne({
+      $or: [
+        { requester: requesterId, recipient: recipientId },
+        { requester: recipientId, recipient: requesterId },
+      ],
+    }).lean();
+
+    if (existingRequest) {
+      return next(new AppError("Friend request or friendship already exists", 400));
+    }
+
+    const newRequest = await Friend.create({
+      requester: requesterId,
+      recipient: recipientId,
+      status: "pending",
+    });
+
+    await createNotification({
+      recipient: recipientId,
+      sender: requesterId,
+      type: "friend_request",
+    });
+
+    return res.status(201).json(jsend.success({ friendRequest: newRequest }));
   }
-
-  const existingRequest = await Friend.findOne({
-    $or: [
-      { requester: requesterId, recipient: recipientId },
-      { requester: recipientId, recipient: requesterId },
-    ],
-  }).lean();
-
-  if (existingRequest) {
-    return next(new AppError("Friend request or friendship already exists", 400));
-  }
-
-  const newRequest = await Friend.create({
-    requester: requesterId,
-    recipient: recipientId,
-    status: "pending",
-  });
-
-  await createNotification({
-    recipient: recipientId,
-    sender: requesterId,
-    type: "friend_request",
-  });
-
-  return res.status(201).json(jsend.success({ friendRequest: newRequest }));
-});
-
+);
 
 export const cancelFriendRequest = asyncWrapper(async (req: Request, res: Response, next: NextFunction) => {
   const user = req.user!;
